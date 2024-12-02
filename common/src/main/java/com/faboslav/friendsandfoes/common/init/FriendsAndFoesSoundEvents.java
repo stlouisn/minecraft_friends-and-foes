0d49b2a0fd42e752946836d8df0513f0ac8d8e68
stlouisn <stlouisn@hotmail.com>
2024-12-02 09:59:51 -0400

    Update FriendsAndFoesSoundEvents.java

--- common/src/main/java/com/faboslav/friendsandfoes/common/init/FriendsAndFoesSoundEvents.java
+++ common/src/main/java/com/faboslav/friendsandfoes/common/init/FriendsAndFoesSoundEvents.java
@@ -40,10 +40,6 @@ public final class FriendsAndFoesSoundEvents
 	public static final RegistryEntry<SoundEvent> ENTITY_ICEOLOGER_HURT = registerSoundEvent("entity.iceologer.hurt");
 	public static final RegistryEntry<SoundEvent> ENTITY_ICEOLOGER_PREPARE_SLOWNESS = registerSoundEvent("entity.iceologer.prepare_slowness");
 	public static final RegistryEntry<SoundEvent> ENTITY_ICEOLOGER_PREPARE_SUMMON = registerSoundEvent("entity.iceologer.prepare_summon");
-	public static final RegistryEntry<SoundEvent> ENTITY_MAULER_BITE = registerSoundEvent("entity.mauler.bite");
-	public static final RegistryEntry<SoundEvent> ENTITY_MAULER_DEATH = registerSoundEvent("entity.mauler.death");
-	public static final RegistryEntry<SoundEvent> ENTITY_MAULER_GROWL = registerSoundEvent("entity.mauler.growl");
-	public static final RegistryEntry<SoundEvent> ENTITY_MAULER_HURT = registerSoundEvent("entity.mauler.hurt");
 	public static final RegistryEntry<SoundEvent> ENTITY_MOOBLOOM_CONVERT = registerSoundEvent("entity.moobloom.convert");
 	public static final RegistryEntry<SoundEvent> ENTITY_PLAYER_MIRROR_MOVE = registerSoundEvent("entity.player.mirror_move");
 	public static final RegistryEntry<SoundEvent> ENTITY_RASCAL_AMBIENT = registerSoundEvent("entity.rascal.ambient");
@@ -61,15 +57,6 @@ public final class FriendsAndFoesSoundEvents
 	public static final RegistryEntry<SoundEvent> ENTITY_TUFF_GOLEM_SLEEP = registerSoundEvent("entity.tuff_golem.sleep");
 	public static final RegistryEntry<SoundEvent> ENTITY_TUFF_GOLEM_STEP = registerSoundEvent("entity.tuff_golem.step");
 	public static final RegistryEntry<SoundEvent> ENTITY_TUFF_GOLEM_WAKE = registerSoundEvent("entity.tuff_golem.wake");
-	public static final RegistryEntry<SoundEvent> ENTITY_WILDFIRE_SHIELD_DEBRIS_IMPACT = registerSoundEvent("entity.shield_debris.impact");
-	public static final RegistryEntry<SoundEvent> ENTITY_WILDFIRE_AMBIENT = registerSoundEvent("entity.wildfire.ambient");
-	public static final RegistryEntry<SoundEvent> ENTITY_WILDFIRE_DEATH = registerSoundEvent("entity.wildfire.death");
-	public static final RegistryEntry<SoundEvent> ENTITY_WILDFIRE_HURT = registerSoundEvent("entity.wildfire.hurt");
-	public static final RegistryEntry<SoundEvent> ENTITY_WILDFIRE_SHIELD_BREAK = registerSoundEvent("entity.wildfire.shield_break");
-	public static final RegistryEntry<SoundEvent> ENTITY_WILDFIRE_SHOCKWAVE = registerSoundEvent("entity.wildfire.shockwave");
-	public static final RegistryEntry<SoundEvent> ENTITY_WILDFIRE_SHOOT = registerSoundEvent("entity.wildfire.shoot");
-	public static final RegistryEntry<SoundEvent> ENTITY_WILDFIRE_STEP = registerSoundEvent("entity.wildfire.step");
-	public static final RegistryEntry<SoundEvent> ENTITY_WILDFIRE_SUMMON_BLAZE = registerSoundEvent("entity.wildfire.summon_blaze");
 
 	private static RegistryEntry<SoundEvent> registerSoundEvent(String path) {
 		return SOUND_EVENTS.register(path, () -> SoundEvent.of(FriendsAndFoes.makeID(path)));
